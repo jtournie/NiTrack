c7bc9a60f020100070f4fa5209d9498b5d81b926
jtournie <jeancharles.tournier@gmail.com>
2014-12-26 22:31:24 +0100

    Remove the need to keep the screen on when the alarm rings

--- app/src/main/java/com/jtournie/cml/tasitrack/AlarmActivity.java
+++ app/src/main/java/com/jtournie/cml/tasitrack/AlarmActivity.java
@@ -91,7 +91,6 @@ public class AlarmActivity extends Activity {
         }
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
     }
